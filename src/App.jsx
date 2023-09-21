e971394909d9a398c8a6c617f8bf4de72ceddb64
HarshPatel682 <patel4harsh@gmail.com>
2023-09-20 22:56:54 -0400

    website is loading fully and made some edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
+import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Works } from './components';
 // import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
 const App = () => {
@@ -15,9 +15,7 @@ const App = () => {
 
         <About />
         <Experience />
-        <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
