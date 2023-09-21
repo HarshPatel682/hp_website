81227cd0d03b787461e7b270128c89dcd19a7877
HarshPatel682 <patel4harsh@gmail.com>
2023-09-20 22:07:47 -0400

    tilt is giving issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { Hero, Navbar } from './components';
+//import { About, Contact, Experience, Feedbacks, Hero, NavBar, Tech, Works, StarsCanvas} from './components';
+
 const App = () => {
 
   return (
-    <div>
-      3d developer
-    </div>
+    <BrowserRouter>
+    asdfasd
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
